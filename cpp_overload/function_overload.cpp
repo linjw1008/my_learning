b372af4429ba4f8551bd88a7c1d5c09a4c819924
linjw1008 <linjw1008@163.com>
2019-07-22 22:12:11 +0800

    函数重载实例

---
+++ cpp_overload/function_overload.cpp
@@ -0,0 +1,35 @@
+﻿//函数重载
+
+#include <iostream>
+
+using namespace std;
+
+class printData
+{
+    public:
+        void print(int i)
+        {
+            cout << "整数为： " << i << endl;
+        }
+
+        void print(double i)
+        {
+            cout << "浮点数为： " << i << endl;
+        }
+
+        void print(char i[])
+        {
+            cout << "字符串为： " << i << endl;
+        }
+};
+
+int main()
+{
+    printData pd;
+
+    pd.print(5);
+    pd.print(544.3654);
+    pd.print((char*)("linjw"));
+
+    system("pause");
+}
