331a1d95790dec0065a65e09d2370f13529c7484
linjw1008 <linjw1008@163.com>
2019-07-22 21:29:40 +0800

    C++拷贝构造函数——函数返回对象时调用的例子

--- cpp_copy_constructor/copy_constructor_V2.cpp
+++ cpp_copy_constructor/copy_constructor_V2.cpp
@@ -11,6 +11,7 @@ public:
 	Line(int len);  //构造函数
 	Line(const Line &obj);  //拷贝构造函数
 	~Line();
+    void setLength(int len);
 
 private:
 	int *ptr;
@@ -33,7 +34,7 @@ Line::Line(const Line &obj)
 	*ptr = *obj.ptr;
 	cout << "拷贝值 *ptr = " << *ptr << endl;
 }
-
+ 
 Line::~Line(void)
 {
 	cout << "释放内存" << endl;
@@ -45,20 +46,46 @@ int Line::getLength(void)
 	return *ptr;
 }
 
+void Line::setLength(int len)
+{
+    *ptr = len;
+}
+
+//对象以值传递方式传入函数体
 void display(Line obj)
 {
 	cout << "Line 长度" << obj.getLength() << endl;
 }
 
+Line createLine(int len)
+{
+	cout << "调用createLine函数返回一个Line对象" << endl;
+	Line ret(len);
+	return ret;
+}
+
 int main()
 {
+    cout << "初始化line1" << endl;
 	Line line1(10);
+    cout << "用line1初始化line2" << endl;
     Line line2 = line1;
+    
+    cout << "更改line1的*ptr" << endl;
+    line1.setLength(20);
+	cout << "line1 *ptr = " << line1.getLength() << endl;
+	cout << "line2 *ptr = " << line2.getLength() << endl;
 
 	//display(line1);
+    cout << "line2以值传递的方式传入函数体" << endl;
     display(line2);
+
+	Line line3 = createLine(15);
+	cout << "返回的line对象line3的*ptr = " << line3.getLength() << endl;
 
 	system("pause");
 
 	return 0;
+
+    
 }
